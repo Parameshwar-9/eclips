365b637f8db4ac715fc4279e31b38cdb931f9533
Parameshwar-9 <Paramrama9@gmail.com>
2025-05-19 23:29:08 +0530

    hi today

--- src/git1/Git132.java
+++ src/git1/Git132.java
@@ -5,6 +5,15 @@ public class Git132 {
 	public static void main(String[]arg) {
 	
 		int[] n={2,6,8,9,7};
-		
+		int temp=0;
+		for(int i=0;i<n.length;i++) {
+			for(int j=i+1;j<n.length;j++) {
+				if(n[i]>n[j]) {
+					temp=n[i];
+					n[i]=n[j];
+					n[j]=temp;
+				}
+			}
+		}
 	}	
 }
