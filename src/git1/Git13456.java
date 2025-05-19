526e667a2ef76ea3c7a68802ce4cc996d457ce8e
Parameshwar-9 <Paramrama9@gmail.com>
2025-05-20 02:04:27 +0530

    Merge branch 'master' of https://github.com/Parameshwar-9/eclips into BranchX
    "iam created new bch"

---
+++ src/git1/Git13456.java
@@ -0,0 +1,22 @@
+package git1;
+
+public class Git13456 {
+
+	public static void main(String[]arg) {
+	
+		int[] n={2,6,8,9,7};
+		int temp=0;
+		for(int i=0;i<n.length;i++) {
+			for(int j=i+1;j<n.length;j++) {
+				if(n[i]>n[j]) {
+					temp=n[i];
+					n[i]=n[j];
+					n[j]=temp;
+				}
+			}
+		}
+		
+			System.out.println(n[2]);
+		
+	}	
+}
